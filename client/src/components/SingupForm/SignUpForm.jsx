843a145382de0b61e39e3907d1de1173d742996d
BrunoFrad <brunfradrib@gmail.com>
2025-01-21 18:48:43 -0300

    Criação da pagina de registrar
    Criação da pagina de registrar e algumas correções de form.

--- client/src/components/SingupForm/SignUpForm.jsx
+++ client/src/components/SingupForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-export default function LoginForm() {
+export default function SignUpForm() {
     
     function handleSubmit() {
         localStorage.setItem("login", true)
@@ -7,8 +7,8 @@ export default function LoginForm() {
     return(
 
         <form className="flex items-center font-bold text-neutral-300 bg-neutral-800 w-5/6 h-5/6 rounded-3xl shadow-xl" action="http://localhost:3000/api/login" method="GET">
-            <div className="flex flex-col items-center justify-evenly h-4/6 w-full">
-                <h1 className="text-6xl">Entrar</h1>
+            <div className="flex flex-col items-center justify-evenly h-5/6 w-full">
+                <h1 className="text-6xl">Registrar</h1>
                 <label className="input input-bordered flex items-center gap-2 bg-neutral-800">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -28,6 +28,16 @@ export default function LoginForm() {
                         <path d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" />
                     </svg>
                     <input type="password" className="grow" placeholder="Senha" />
+                </label>
+                <label className="input input-bordered flex items-center gap-2 bg-neutral-800">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" />
+                    </svg>
+                    <input type="password" className="grow" placeholder="Confirmar a senha" />
                 </label>
                 <button className="btn btn-primary w-40 text-neutral-300" type="submit" onClick={handleSubmit}>Submit</button>
             </div>
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,15 +1,38 @@
 import Navbar from "../../components/Navbar/Navbar"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import SignUpForm from "../../components/SingupForm/SignUpForm"
 
 export default function Login() {
-  return(
-    <article className="flex">
-        <Navbar />
-        <section className="flex w-full justify-center">
-            <section className="flex w-5/12 justify-center items-center">
-                <LoginForm />
+
+  if(localStorage.getItem("login") === null) {
+    localStorage.setItem("login", false)
+  }else {
+
+    if(localStorage.getItem("login") === "false") {
+      return(
+        <article className="flex">
+            <Navbar />
+            <section className="flex w-full justify-center">
+                <section className="flex w-5/12 justify-center items-center">
+                    <SignUpForm />
+                </section>
             </section>
-        </section>
-    </article>
-  )
+        </article>
+      )
+    }else {
+      return(
+        <article className="flex">
+            <Navbar />
+            <section className="flex w-full justify-center">
+                <section className="flex w-5/12 justify-center items-center">
+                    <LoginForm />
+                </section>
+            </section>
+        </article>
+      )
+    }
+
+  }
+
+  
 }
