9b25c2afda34b1fb9f4c57e1f6847846094c19bc
BrunoFrad <brunfradrib@gmail.com>
2025-01-25 13:15:14 -0300

    Criei a pagina Home
    Criei a Home com os botoes para criar uma nova playlist ou editar uma playlist

--- client/src/components/FeatureBox/FeatureBox.jsx
+++ client/src/components/FeatureBox/FeatureBox.jsx
@@ -1,5 +1,9 @@
-export default function FeatureBox() {
-
-
-
+export default function FeatureBox({title}) {
+    return (
+        <div class="card w-76 shadow-md bg-neutral-800">
+            <div class="card-body text-center hover:cursor-pointer hover:border-neutral-50 hover:border-2 hover:border-solid hover:rounded-2xl">
+                <h2 class="card-title hover:cursor-pointer">{title}</h2>
+            </div>
+        </div>
+    )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../../components/Navbar/Navbar"
+import FeatureBox from "../../components/FeatureBox/FeatureBox"
 
 export default function App() {
 
@@ -9,7 +10,16 @@ export default function App() {
       window.location.replace("http://localhost:5173/need-login.html")
     }else {
       return(
-        <Navbar />
+        <div className="flex">
+          <Navbar />
+          <div className="flex flex-col w-full justify-center items-center gap-20">
+            <h1 className="text-6xl font-bold text-neutral-500">Bem-Vindo ao Fusyc</h1>
+            <div className="flex flex-row justify-evenly w-3/6">
+              <FeatureBox title={"Criar nova playlist! 🎉"} />
+              <FeatureBox title={"Editar uma playlist! 📝"} />
+            </div>
+          </div>
+        </div>
       )
     }
   }
